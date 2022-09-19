a45bc934edf3db9be2a0fad0394bb75a89666fbc
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2022-09-19 19:08:21 -0400

    change initial size number

--- src/lab/lab1A/Part2AnsSolver.java
+++ src/lab/lab1A/Part2AnsSolver.java
@@ -18,7 +18,7 @@ public class Part2AnsSolver {
     }
     static void findLargestSize()
     {
-        long max1=0,max2=0,maxtime1=0,maxtime2=0;
+        long max1=500000,max2=500000,maxtime1=0,maxtime2=0;
         while (true)
         {
             max1++;
