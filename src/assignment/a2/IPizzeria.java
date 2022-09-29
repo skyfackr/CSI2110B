88e1997a0a5cf1b0d6cc58c54114a1cb49858a07
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2022-09-29 15:53:20 -0400

    create interface for functions question asked

---
+++ src/assignment/a2/IPizzeria.java
@@ -0,0 +1,7 @@
+package assignment.a2;
+
+public interface IPizzeria {
+    void addPizza(Pizza pizza);
+    Pizza getPizza(int kind);
+    Pizza getSurprisePizza();
+}
--- src/assignment/a2/Pizzeria.java
+++ src/assignment/a2/Pizzeria.java
@@ -1,4 +1,23 @@
 package assignment.a2;
 
-public class Pizzeria {
+import java.util.Queue;
+
+public class Pizzeria implements IPizzeria {
+    Queue<Pizza> queue;
+    static int M=10;
+
+    @Override
+    public void addPizza(Pizza pizza) {
+
+    }
+
+    @Override
+    public Pizza getPizza(int kind) {
+        return null;
+    }
+
+    @Override
+    public Pizza getSurprisePizza() {
+        return null;
+    }
 }
