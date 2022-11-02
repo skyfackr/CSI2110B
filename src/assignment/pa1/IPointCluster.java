49d12754ad728276d2f4187b0880217fd7a5ba59
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2022-11-01 21:17:42 -0400

    pointcluster interfacing

---
+++ src/assignment/pa1/IPointCluster.java
@@ -0,0 +1,15 @@
+package assignment.pa1;
+
+import java.util.List;
+
+public interface IPointCluster {
+    void addPoint(IPoint3D point);
+
+    List<IPoint3D> getPoints();
+
+    int getClusterId();
+
+    String getRGB();
+
+    boolean removePoint(IPoint3D point);
+}
--- src/assignment/pa1/PointCluster.java
+++ src/assignment/pa1/PointCluster.java
@@ -3,7 +3,7 @@ package assignment.pa1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PointCluster {
+public class PointCluster implements IPointCluster {
     private final List<IPoint3D> points;
     private final int clusterId;
     private final double r;
@@ -16,19 +16,24 @@ public class PointCluster {
         this.g = g;
         this.b = b;
     }
+    @Override
     public void addPoint(IPoint3D point) {
         points.add(point);
         point.setCluster(clusterId);
     }
+    @Override
     public List<IPoint3D> getPoints() {
         return points;
     }
+    @Override
     public int getClusterId() {
         return clusterId;
     }
+    @Override
     public String getRGB() {
         return String.format("%.1f,%.1f,%.1f", r , g , b );
     }
+    @Override
     public boolean removePoint(IPoint3D point) {
         if (points.remove(point)) {
             point.setCluster(-1);
