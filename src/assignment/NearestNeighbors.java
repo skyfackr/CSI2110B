5b3cad389982ea5d8fb148ef096f1662f0e70266
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2022-11-01 20:47:34 -0400

    Create NearestNeighbors.java

---
+++ src/assignment/NearestNeighbors.java
@@ -0,0 +1,16 @@
+package assignment;
+
+import assignment.pa1.IPoint3D;
+
+import java.util.List;
+
+public class NearestNeighbors {
+    private final List<IPoint3D> lists;
+    public NearestNeighbors(List<IPoint3D> lists) {
+        this.lists = lists;
+    }
+    public List<IPoint3D> rangeQuery(double eps, IPoint3D point) {
+        //todo a rangeQuery method that finds the nearest neighbors of a 3D point
+        throw new UnsupportedOperationException();
+    }
+}
