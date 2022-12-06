c680f5836e5fc51fac14fb56da546b175d336e18
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2022-12-05 22:26:13 -0500

    algorithm

--- src/assignment/pa2/KDTree.java
+++ src/assignment/pa2/KDTree.java
@@ -1,9 +1,5 @@
 package assignment.pa2;
 
-import assignment.pa2.Point3D;
-
-import java.util.List;
-
 public class KDTree {
     protected static int DIMENSION = 3;
     private KDTreeNode root;
@@ -11,6 +7,26 @@ public class KDTree {
     public KDTree() {
         root = null;
     }
+
+    public KDTreeNode getRoot() {
+        return root;
+    }
+
+    public KDTreeNode add(Point3D p) {
+        return insertInternal(root,p ,0);
+    }
+    private KDTreeNode insertInternal(KDTreeNode node, Point3D p, int depth) {
+        if (node == null) {
+            return new KDTreeNode(p, depth );
+        }
+        if (p.get(node.axis) <= node.value) {
+            node.left = insertInternal(node.left, p, (depth + 1)% DIMENSION);
+        } else {
+            node.right = insertInternal(node.right, p, (depth + 1)% DIMENSION);
+        }
+        return node;
+    }
+
     public static class KDTreeNode
     {
         public final Point3D point;
---
+++ src/assignment/pa2/NearestNeighborsKD.java
@@ -0,0 +1,39 @@
+package assignment.pa2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NearestNeighborsKD {
+    protected KDTree tree;
+
+    // construct with list of points
+    public NearestNeighborsKD(List<Point3D> points) {
+        this.tree = new KDTree();
+        //List<Point3D> neighbors;
+        for (Point3D p : points) {
+            this.tree.add(p); // the add method should call the
+// insert method given in pseudo-code
+        }
+// plus possibly other initializations
+    }
+
+    public List<Point3D> rangeQuery(Point3D p, double eps) {
+        List<Point3D> neighbors= new ArrayList<Point3D>();
+        rangeQuery(p, eps, neighbors, tree.getRoot());
+        return neighbors;
+    }
+    private void rangeQuery(Point3D p, double eps, List<Point3D> neighbors, KDTree.KDTreeNode node) {
+        if (node == null) {
+            return;
+        }
+        if (p.distance(node.point) < eps) {
+            neighbors.add(node.point);
+        }
+        if (p.get(node.axis) -eps<= node.value) {
+            rangeQuery(p, eps, neighbors, node.left);
+        }
+        if (p.get(node.axis) +eps> node.value) {
+            rangeQuery(p, eps, neighbors, node.right);
+        }
+    }
+}
